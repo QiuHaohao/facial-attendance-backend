5c7499b10f7cad47c015f24998d68a670b3463ee
Fang Meiyi <fang0074@e.ntu.edu.sg>
2019-11-13 15:59:39 +0800

    update security/tests.py

--- security/tests.py
+++ security/tests.py
@@ -15,3 +15,19 @@ class SignUpViewTest(TestCase):
                          {'username': username, 'password': password},
                          content_type="application/json")
         self.assertTrue(User.objects.filter(username=username).exists())
+
+        response = self.client.post(reverse('signup'),
+                         {'username': username, 'password': password},
+                         content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+
+
+class CurrentUserTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='admin', email='admin@example.com', password='adminpassword')
+        self.client.login(username='admin', password='adminpassword')
+
+    def test_current_user(self):
+        response = self.client.get(reverse('current_user').format(self.user.id), follow=True)
+        self.assertEqual(response.status_code, 200)
